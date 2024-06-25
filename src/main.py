9bcab058b350b09daf421496aefbb710f9543204
mirdukkkkk <69540930+mirdukkkkk@users.noreply.github.com>
2024-06-26 09:24:12 +1200

    add auto timelapse & neighboor flag

--- src/main.py
+++ src/main.py
@@ -18,7 +18,11 @@ async def record(output_file):
         try:
             data = json.loads(message)
             if 'x' in data and 'y' in data and 'color' in data:
-                change = {'x': data['x'], 'y': data['y'], 'color': data['color']}
+                change = { 
+                    'x': data['x'], 
+                    'y': data['y'], 
+                    'color': data['color'] 
+                }
                 recorder.record_change(change)
             else:
                 print(f'Ignoring unexpected message format: {data}')
@@ -35,37 +39,48 @@ async def record(output_file):
 
     recorder.close()
 
-def parse(ffmpeg_path, input_file, output_file, fps, scale_factor=1):
+def parse(input_file, output_file, fps, scale_factor, frame_per_pixel):
     initial_image = input_file.replace('.db', '_initial.png')
-    
-    video_creator = TimelapseVideoCreator(ffmpeg_path, initial_image, input_file, output_file, fps, scale_factor)
+    ffmpeg_path = Configuration('config.json').get('ffmpeg_path', 'ffmpeg')
+
+    video_creator = TimelapseVideoCreator(ffmpeg_path, initial_image, input_file, output_file, fps, scale_factor, frame_per_pixel)
     video_creator.create_video()
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print('Usage: py main.py <record|parse> <input_file> [<output_file>] [-x <scale_factor>]')
+        print('Usage: py main.py <record|parse> <input_file> [<output_file>] [-f <fps>] [-x <scale_factor>] [-o]')
         sys.exit(1)
-    
+
     mode = sys.argv[1]
     input_file = sys.argv[2]
     scale_factor = 1
+    frame_per_pixel = False
+    fps = 30
 
     if mode == 'record':
         asyncio.run(record(input_file))
     elif mode == 'parse':
         if len(sys.argv) < 4:
-            print('Usage: py main.py parse <input_file> <output_file> [-x <scale_factor>]')
+            print('Usage: py main.py parse <input_file> <output_file> [-f <fps>] [-x <scale_factor>] [-o]')
             sys.exit(1)
+
         output_file = sys.argv[3]
+
+        if '-f' in sys.argv:
+            fps_index = sys.argv.index('-f') + 1
+            if fps_index < len(sys.argv):
+                fps = int(sys.argv[fps_index])
+
         if '-x' in sys.argv:
             scale_factor_index = sys.argv.index('-x') + 1
             if scale_factor_index < len(sys.argv):
                 scale_factor = int(sys.argv[scale_factor_index])
-        config = Configuration('config.json')
-        fps = config.get('fps', 30)
-        ffmpeg_path = config.get('ffmpeg_path')
-        parse(ffmpeg_path, input_file, output_file, fps, scale_factor)
+
+        if '-o' in sys.argv:
+            frame_per_pixel = True
+
+        parse(input_file, output_file, fps, scale_factor, frame_per_pixel)
     else:
         print('Unknown mode:', mode)
-        print('Usage: py main.py <record|parse> <input_file> [<output_file>] [-x <scale_factor>]')
+        print('Usage: py main.py <record|parse> <input_file> [<output_file>] [-f <fps>] [-x <scale_factor>] [-o]')
         sys.exit(1)
--- src/services/video_creator.py
+++ src/services/video_creator.py
@@ -4,13 +4,14 @@ import io
 import subprocess
 
 class TimelapseVideoCreator:
-    def __init__(self, ffmpeg_path, initial_canvas_image, db_file, output_video, fps=30, scale_factor=1):
+    def __init__(self, ffmpeg_path, initial_canvas_image, db_file, output_video, fps, scale_factor, frame_per_pixel):
         self.initial_canvas_image = initial_canvas_image
         self.ffmpeg_path = ffmpeg_path
         self.db_file = db_file
         self.output_video = output_video
         self.fps = fps
         self.scale_factor = scale_factor
+        self.frame_per_pixel = frame_per_pixel
 
     def create_video(self):
         initial_image = Image.open(self.initial_canvas_image)
@@ -20,7 +21,7 @@ class TimelapseVideoCreator:
         cursor.execute("SELECT timestamp, x, y, color FROM changes ORDER BY timestamp")
 
         try:
-            scale_option = f'scale=iw*{self.scale_factor}:ih*{self.scale_factor}' if self.scale_factor != 1 else 'null'
+            scale_option = f'scale=iw*{self.scale_factor}:ih*{self.scale_factor}:flags=neighbor' if self.scale_factor != 1 else 'null'
             process = subprocess.Popen(
                 [self.ffmpeg_path, '-y', '-f', 'image2pipe', '-vcodec', 'png', '-r', str(self.fps), '-i', '-', 
                     '-vf', scale_option, '-vcodec', 'libx264', '-pix_fmt', 'yuv420p', self.output_video],
@@ -35,7 +36,7 @@ class TimelapseVideoCreator:
         previous_timestamp = None
 
         for timestamp, x, y, color in cursor:
-            if previous_timestamp is not None:
+            if previous_timestamp is not None and not self.frame_per_pixel:
                 frame_duration = (timestamp - previous_timestamp) / 1000
                 frame_count = int(frame_duration * self.fps)
 
@@ -46,6 +47,10 @@ class TimelapseVideoCreator:
                     process.stdin.write(buffer.getvalue())
 
             draw.point((x, y), fill=color)
+            if self.frame_per_pixel:
+                buffer = io.BytesIO()
+                current_image.save(buffer, format='PNG')
+                process.stdin.write(buffer.getvalue())
             previous_timestamp = timestamp
 
         buffer = io.BytesIO()
